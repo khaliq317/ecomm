77ee41fbb58c19b04426243bf0d34fb85393360b
Khaliq Abbas <140581171+khaliq317@users.noreply.github.com>
2024-02-17 09:58:43 +0500

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header"
+import Footer from "@/components/footer"
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -25,6 +26,7 @@ export default function RootLayout({
         <header/>
         
         {children}
+        <Footer/>
         
         </body>
     </html>
